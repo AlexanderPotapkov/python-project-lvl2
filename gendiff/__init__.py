7219a274069439c62d274e124dbd4760e8d955dc
Alexander Potapkov <xanben@mail.ru>
2022-04-19 22:10:39 +0300

    step 3

---
+++ gendiff/__init__.py
@@ -0,0 +1,3 @@
+from gendiff.generate_difference import generate_diff
+
+__all__ = ('generate_diff',)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
+
+from gendiff.generate_difference import generate_diff
+from pathlib import Path
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=Path)
+    parser.add_argument('second_file', type=Path)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
