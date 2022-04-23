26bba82c657c2b9845cb48c5c70b076bb9932f0f
Alexander Potapkov <xanben@mail.ru>
2022-04-23 10:49:55 +0300

    add yaml files in fixtures and add tests to test_generate_difference.py

--- tests/test_generate_difference.py
+++ tests/test_generate_difference.py
@@ -1,9 +1,45 @@
+import pytest
 from gendiff.generate_difference import generate_diff
 
-JSON_1 = 'tests/fixtures/file_1.json'
-JSON_2 = 'tests/fixtures/file_2.json'
+@pytest.fixture
+def json_path_1():
+    return 'tests/fixtures/file_1.json'
 
 
-def test_generate_diff():
-    correct_answer = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
-    assert generate_diff(JSON_1, JSON_2) == correct_answer
+@pytest.fixture
+def json_path_2():
+    return 'tests/fixtures/file_2.json'
+
+
+def test_json(json_path_1, json_path_2):
+    excepted = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+    assert generate_diff(json_path_1, json_path_2) == excepted
+
+
+@pytest.fixture
+def yaml_path_1():
+    return 'tests/fixtures/file_1.yaml'
+
+
+@pytest.fixture
+def yaml_path_2():
+    return 'tests/fixtures/file_2.yaml'
+
+
+def test_yaml(yaml_path_1, yaml_path_2):
+    excepted = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+    assert generate_diff(yaml_path_1, yaml_path_2) == excepted
