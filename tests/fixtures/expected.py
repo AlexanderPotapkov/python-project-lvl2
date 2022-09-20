13a2a75458620b243f9072de7c1019c2ca6c20f4
alexander_potapkov <xanben@mail.ru>
2022-09-20 20:12:50 +0300

    fix linter problems

--- tests/fixtures/expected.py
+++ tests/fixtures/expected.py
@@ -1,76 +1,76 @@
 # -*- coding: utf-8 -*-
 
-SIMPLE_STYLISH = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+SIMPLE_STYLISH = ("{\n"
+                  "  - follow: false\n"
+                  "    host: hexlet.io\n"
+                  "  - proxy: 123.234.53.22\n"
+                  "  - timeout: 50\n"
+                  "  + timeout: 20\n"
+                  "  + verbose: true\n"
+                  "}")
 
-COMPLEX_STYLISH = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+COMPLEX_STYLISH = ("{\n"
+                   "    common: {\n"
+                   "      + follow: false\n"
+                   "        setting1: Value 1\n"
+                   "      - setting2: 200\n"
+                   "      - setting3: true\n"
+                   "      + setting3: null\n"
+                   "      + setting4: blah blah\n"
+                   "      + setting5: {\n"
+                   "            key5: value5\n"
+                   "        }\n"
+                   "        setting6: {\n"
+                   "            doge: {\n"
+                   "              - wow: \n"
+                   "              + wow: so much\n"
+                   "            }\n"
+                   "            key: value\n"
+                   "          + ops: vops\n"
+                   "        }\n"
+                   "    }\n"
+                   "    group1: {\n"
+                   "      - baz: bas\n"
+                   "      + baz: bars\n"
+                   "        foo: bar\n"
+                   "      - nest: {\n"
+                   "            key: value\n"
+                   "        }\n"
+                   "      + nest: str\n"
+                   "    }\n"
+                   "  - group2: {\n"
+                   "        abc: 12345\n"
+                   "        deep: {\n"
+                   "            id: 45\n"
+                   "        }\n"
+                   "    }\n"
+                   "  + group3: {\n"
+                   "        deep: {\n"
+                   "            id: {\n"
+                   "                number: 45\n"
+                   "            }\n"
+                   "        }\n"
+                   "        fee: 100500\n"
+                   "    }\n"
+                   "}")
 
-SIMPLE_PLAIN = """Property 'follow' was removed
-Property 'proxy' was removed
-Property 'timeout' was updated. From 50 to 20
-Property 'verbose' was added with value: true"""
+SIMPLE_PLAIN = ("Property 'follow' was removed\n"
+                "Property 'proxy' was removed\n"
+                "Property 'timeout' was updated. From 50 to 20\n"
+                "Property 'verbose' was added with value: true")
 
-COMPLEX_PLAIN = """Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]"""
+COMPLEX_PLAIN = ("Property 'common.follow' was added with value: false\n"
+                 "Property 'common.setting2' was removed\n"
+                 "Property 'common.setting3' was updated. From true to null\n"
+                 "Property 'common.setting4' was added with value: 'blah blah'\n"
+                 "Property 'common.setting5' was added with value: [complex value]\n"
+                 "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
+                 "Property 'common.setting6.ops' was added with value: 'vops'\n"
+                 "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+                 "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+                 "Property 'group2' was removed\n"
+                 "Property 'group3' was added with value: [complex value]")
 
-SIMPLE_JSON = """{"follow": {"status": "removed", "value": false}, "host": {"status": "not changed", "value": "hexlet.io"}, "proxy": {"status": "removed", "value": "123.234.53.22"}, "timeout": {"status": "updated", "value1": 50, "value2": 20}, "verbose": {"status": "added", "value": true}}"""
+SIMPLE_JSON = "{\"follow\": {\"status\": \"removed\", \"value\": false}, \"host\": {\"status\": \"not changed\", \"value\": \"hexlet.io\"}, \"proxy\": {\"status\": \"removed\", \"value\": \"123.234.53.22\"}, \"timeout\": {\"status\": \"updated\", \"value1\": 50, \"value2\": 20}, \"verbose\": {\"status\": \"added\", \"value\": true}}"
 
-COMPLEX_JSON = """{"common": {"status": "nested", "children": {"follow": {"status": "added", "value": false}, "setting1": {"status": "not changed", "value": "Value 1"}, "setting2": {"status": "removed", "value": 200}, "setting3": {"status": "updated", "value1": true, "value2": null}, "setting4": {"status": "added", "value": "blah blah"}, "setting5": {"status": "added", "value": {"key5": "value5"}}, "setting6": {"status": "nested", "children": {"doge": {"status": "nested", "children": {"wow": {"status": "updated", "value1": "", "value2": "so much"}}}, "key": {"status": "not changed", "value": "value"}, "ops": {"status": "added", "value": "vops"}}}}}, "group1": {"status": "nested", "children": {"baz": {"status": "updated", "value1": "bas", "value2": "bars"}, "foo": {"status": "not changed", "value": "bar"}, "nest": {"status": "updated", "value1": {"key": "value"}, "value2": "str"}}}, "group2": {"status": "removed", "value": {"abc": 12345, "deep": {"id": 45}}}, "group3": {"status": "added", "value": {"deep": {"id": {"number": 45}}, "fee": 100500}}}"""
+COMPLEX_JSON = "{\"common\": {\"status\": \"nested\", \"children\": {\"follow\": {\"status\": \"added\", \"value\": false}, \"setting1\": {\"status\": \"not changed\", \"value\": \"Value 1\"}, \"setting2\": {\"status\": \"removed\", \"value\": 200}, \"setting3\": {\"status\": \"updated\", \"value1\": true, \"value2\": null}, \"setting4\": {\"status\": \"added\", \"value\": \"blah blah\"}, \"setting5\": {\"status\": \"added\", \"value\": {\"key5\": \"value5\"}}, \"setting6\": {\"status\": \"nested\", \"children\": {\"doge\": {\"status\": \"nested\", \"children\": {\"wow\": {\"status\": \"updated\", \"value1\": \"\", \"value2\": \"so much\"}}}, \"key\": {\"status\": \"not changed\", \"value\": \"value\"}, \"ops\": {\"status\": \"added\", \"value\": \"vops\"}}}}}, \"group1\": {\"status\": \"nested\", \"children\": {\"baz\": {\"status\": \"updated\", \"value1\": \"bas\", \"value2\": \"bars\"}, \"foo\": {\"status\": \"not changed\", \"value\": \"bar\"}, \"nest\": {\"status\": \"updated\", \"value1\": {\"key\": \"value\"}, \"value2\": \"str\"}}}, \"group2\": {\"status\": \"removed\", \"value\": {\"abc\": 12345, \"deep\": {\"id\": 45}}}, \"group3\": {\"status\": \"added\", \"value\": {\"deep\": {\"id\": {\"number\": 45}}, \"fee\": 100500}}}"
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,11 @@
 import pytest
 from gendiff.generate_difference import generate_diff
-from tests.fixtures.expected import *
+from tests.fixtures.expected import (SIMPLE_STYLISH,
+                                     SIMPLE_PLAIN,
+                                     SIMPLE_JSON,
+                                     COMPLEX_STYLISH,
+                                     COMPLEX_PLAIN,
+                                     COMPLEX_JSON)
 
 FILEPATH_JSON_1_1 = 'tests/fixtures/simple_1.json'
 FILEPATH_JSON_1_2 = 'tests/fixtures/simple_2.json'
@@ -11,12 +16,6 @@ FILEPATH_YAML_1_2 = 'tests/fixtures/simple_2.yaml'
 FILEPATH_YAML_2_1 = 'tests/fixtures/complex_1.yaml'
 FILEPATH_YAML_2_2 = 'tests/fixtures/complex_2.yaml'
 FILEPATH_WRONG = 'tests/fixtures/expectation_json.txt'
-EXPECTATION_SIMPLE_STYLISH = SIMPLE_STYLISH
-EXPECTATION_COMPLEX_STYLISH = COMPLEX_STYLISH
-EXPECTATION_SIMPLE_PLAIN = SIMPLE_PLAIN
-EXPECTATION_COMPLEX_PLAIN = COMPLEX_PLAIN
-EXPECTATION_SIMPLE_JSON = SIMPLE_JSON
-EXPECTATION_COMPLEX_JSON = COMPLEX_JSON
 
 
 def test_wrong_file_gendiff():
@@ -30,39 +29,39 @@ def test_wrong_format_gendiff():
     with pytest.raises(TypeError) as err:
         generate_diff(FILEPATH_JSON_2_1, FILEPATH_JSON_2_2, 'unstylish')
 
-    assert str(err. value) == 'Wrong format!'
+    assert str(err.value) == 'Wrong format!'
 
 
 def test_default_format():
     assert (generate_diff(FILEPATH_JSON_1_1,
                           FILEPATH_JSON_1_2) == generate_diff(
-            FILEPATH_JSON_1_1, FILEPATH_JSON_1_2, 'stylish'))
+        FILEPATH_JSON_1_1, FILEPATH_JSON_1_2, 'stylish'))
 
 
 @pytest.mark.parametrize('first_file, second_file, format, expected', [
     (FILEPATH_JSON_1_1, FILEPATH_JSON_1_2,
-     'stylish', EXPECTATION_SIMPLE_STYLISH),
+     'stylish', SIMPLE_STYLISH),
     (FILEPATH_JSON_2_1, FILEPATH_JSON_2_2,
-     'stylish', EXPECTATION_COMPLEX_STYLISH),
+     'stylish', COMPLEX_STYLISH),
     (FILEPATH_JSON_1_1, FILEPATH_JSON_1_2,
-     'plain', EXPECTATION_SIMPLE_PLAIN),
+     'plain', SIMPLE_PLAIN),
     (FILEPATH_JSON_2_1, FILEPATH_JSON_2_2,
-     'plain', EXPECTATION_COMPLEX_PLAIN),
+     'plain', COMPLEX_PLAIN),
     (FILEPATH_JSON_1_1, FILEPATH_JSON_1_2,
-     'json', EXPECTATION_SIMPLE_JSON),
+     'json', SIMPLE_JSON),
     (FILEPATH_JSON_2_1, FILEPATH_JSON_2_2,
-     'json', EXPECTATION_COMPLEX_JSON),
+     'json', COMPLEX_JSON),
     (FILEPATH_YAML_1_1, FILEPATH_YAML_1_2,
-     'stylish', EXPECTATION_SIMPLE_STYLISH),
+     'stylish', SIMPLE_STYLISH),
     (FILEPATH_YAML_2_1, FILEPATH_YAML_2_2,
-     'stylish', EXPECTATION_COMPLEX_STYLISH),
+     'stylish', COMPLEX_STYLISH),
     (FILEPATH_YAML_1_1, FILEPATH_YAML_1_2,
-     'plain', EXPECTATION_SIMPLE_PLAIN),
+     'plain', SIMPLE_PLAIN),
     (FILEPATH_YAML_2_1, FILEPATH_YAML_2_2,
-     'plain', EXPECTATION_COMPLEX_PLAIN),
+     'plain', COMPLEX_PLAIN),
     (FILEPATH_YAML_1_1, FILEPATH_YAML_1_2,
-     'json', EXPECTATION_SIMPLE_JSON),
+     'json', SIMPLE_JSON),
     (FILEPATH_YAML_2_1, FILEPATH_YAML_2_2,
-     'json', EXPECTATION_COMPLEX_JSON,), ])
+     'json', COMPLEX_JSON,), ])
 def test_generate_diff(first_file, second_file, format, expected):
     assert generate_diff(first_file, second_file, format) == expected
