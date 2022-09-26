336a95f5fb8a77bd850aa70d3b3722903887891a
alexander_potapkov <xanben@mail.ru>
2022-09-26 21:58:54 +0300

    fix script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,18 +2,23 @@
 
 import argparse
 import pathlib
-
 from gendiff.generate_difference import generate_diff
 
 
-def main():
+def parsing():
     parser = argparse.ArgumentParser(description='Generate diff',
                                      conflict_handler='resolve')
     parser.add_argument('first_file', type=pathlib.Path)
     parser.add_argument('second_file', type=pathlib.Path)
     parser.add_argument('-f', '--format', default='stylish', metavar='FORMAT',
                         help='set format of output (default: stylish)')
+
     args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parsing()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
